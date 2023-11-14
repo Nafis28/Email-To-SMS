ec5d7df2921c4354e2e8ceae74c1508f734fc8cc
Nafis <30291815+Nafis28@users.noreply.github.com>
2023-11-14 13:38:13 +1100

    Update worker.js
    SID & AUTH Removed from code - env setup
    Updated error response

--- worker.js
+++ worker.js
@@ -2,40 +2,35 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
-const TWILIO_ACCOUNT_SID = 'ID';
-const TWILIO_AUTH_TOKEN = 'ID';
-const TWILIO_PHONE_NUMBER = '+61XXXXXXXX'; //Add Your Twilio Number you will send SMS with
+const TWILIO_PHONE_NUMBER = '+61480092025';
 
 async function handleRequest(request) {
   if (request.method === 'POST') {
-    try {
-      const data = await request.json();
-      const senderEmail = data.sender;
-      const messageBody = data.body;
+    const data = await request.json();
+    const senderEmail = data.sender; 
+    const messageBody = data.body;
 
-      const phoneMappings = {
-        'support@aatroxcommunications.com.au': '+61XXXXXXXX'  // Replace with actual phone number
-        // Add other mappings as needed
-      };
+    // Map sender email to phone number (customize this mapping based on your needs)
+    const phoneMappings = {
+      'support@app.aatroxcommunications.com.au': '+61423624140'  // Replace with actual phone number
+      // Add other mappings as needed
+    };
 
-      const recipientPhoneNumber = phoneMappings[senderEmail];
-      if (recipientPhoneNumber) {
-        const response = await sendSMS(recipientPhoneNumber, messageBody);
-        if (response.ok) {
-          return new Response('SMS sent', { status: 200 });
-        } else {
-          console.error(`Failed to send SMS: ${response.statusText}`);
-          return new Response('Failed to send SMS', { status: 500 });
-        }
+    const recipientPhoneNumber = phoneMappings[senderEmail];
+    if (recipientPhoneNumber) {
+      const smsResponse = await sendSMS(recipientPhoneNumber, messageBody);
+      if (smsResponse.ok) {
+        return new Response('SMS sent', {status: 200});
       } else {
-        return new Response('No mapping found for email', { status: 400 });
+        const errorBody = await smsResponse.text();
+        console.error(`Failed to send SMS: ${smsResponse.statusText}, Response Body: ${errorBody}`);
+        return new Response('Failed to send SMS', {status: smsResponse.status});
       }
-    } catch (error) {
-      console.error(`Error processing request: ${error}`);
-      return new Response('Error processing request', { status: 500 });
+    } else {
+      return new Response('No mapping found for email', {status: 400});
     }
   } else {
-    return new Response('Invalid request method', { status: 405 });
+    return new Response('Invalid request method', {status: 405});
   }
 }
 
